cb857bc557a0b2ac7bbf8463e8f49daf321b9c8d
Jack Hsu <jackhsu0225@gmail.com>
2023-02-15 21:13:13 -0500

    Passing bindings between views to maintain the source of truth

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -1,7 +1,8 @@
 import SwiftUI
 
 struct DetailView: View {
-    let scrum: DailyScrum
+    @Binding var scrum: DailyScrum
+    @State private var data = DailyScrum.Data()
     @State private var isPresentingEditView = false
 
     var body: some View {
@@ -39,11 +40,12 @@ struct DetailView: View {
         .toolbar {
              Button("Edit") {
                  isPresentingEditView = true
+                 data = scrum.data
              }
          }
         .sheet(isPresented: $isPresentingEditView) {
             NavigationView {
-                DetailEditView()
+                DetailEditView(data: $data)
                     .navigationTitle(scrum.title)
                     .toolbar {
                         ToolbarItem(placement: .cancellationAction) {
@@ -52,10 +54,11 @@ struct DetailView: View {
                             }
                         }
                         ToolbarItem(placement: .confirmationAction) {
-                               Button("Done") {
-                                   isPresentingEditView = false
-                               }
-                           }
+                            Button("Done") {
+                               isPresentingEditView = false
+                               scrum.update(from: data)
+                            }
+                        }
                     }
             }
             
@@ -66,7 +69,7 @@ struct DetailView: View {
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
         NavigationView {
-            DetailView(scrum: DailyScrum.sampleData[0])
+            DetailView(scrum: .constant(DailyScrum.sampleData[0]))
         }
     }
 }
