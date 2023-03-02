a0efb69fdc85572b5e994cff5ca9ff949107f858
Jack Hsu <jackhsu0225@gmail.com>
2023-03-01 20:57:10 -0500

    Create a data store

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,14 @@
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                       in: .userDomainMask,
+                                       appropriateFor: nil,
+                                       create: false)
+            .appendingPathComponent("scrums.data") //return the URL of a file named scrums.data.
+    }
+}
